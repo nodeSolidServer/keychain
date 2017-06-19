af596ab42e972d9e417e9a01559b100640997278
Christian Smith <smith@anvil.io>
2017-06-19 18:13:22 -0400

    feat(EcdsaKeyPair): draft implementation

---
+++ src/algorithms/EcdsaKeyPair.js
@@ -0,0 +1,113 @@
+/**
+ * Dependencies
+ */
+const crypto = require('@trust/webcrypto')
+const base64url = require('base64url')
+
+/**
+ * EcdsaKeyPair
+ */
+class EcdsaKeyPair {
+
+  /**
+   * constructor
+   *
+   * @param params {Object} Options hashmap
+   * @param params.alg {string} For example, 'RS256'
+   * @param params.modulusLength {number}
+   * @param params.publicExponent {BufferSource} For example, a Uint8Array
+   * @param params.usages {Array<string>}
+   */
+  constructor (params) {
+    let name = 'ECDSA'
+    let {alg, curve, usages} = params
+    let hashLengthValid = alg.match(/(256|384|512)$/)
+    let hashLength = hashLengthValid && hashLengthValid.shift()
+    let namedCurve = `K-${hashLength}`
+    let hash = { name: `SHA-${hashLength}` }
+
+    if (!hashLength) {
+      throw new Error('Invalid hash length')
+    }
+
+    if (!usages) {
+      usages = ['sign', 'verify']
+    }
+
+    this.algorithm = {name, namedCurve, hash}
+    this.extractable = true
+    this.usages = usages
+  }
+
+  /**
+   * generateKey
+   */
+  generateKey () {
+    let {algorithm, extractable, usages} = this
+
+    return crypto.subtle
+      .generateKey(algorithm, extractable, usages)
+      .then(this.setCryptoKeyPair)
+      .then(this.setJwkKeyPair)
+  }
+
+  /**
+   * importKey
+   */
+  importKey (jwk) {
+    let {name, hash} = this.algorithm
+    let algorithm = {name, hash}
+    let extractable = true
+    let usages = jwk.key_ops
+
+    return crypto.subtle
+      .importKey('jwk', jwk, algorithm, extractable, usages)
+  }
+
+  /**
+   * setCryptoKeyPair
+   */
+  setCryptoKeyPair (cryptoKeyPair) {
+    let result = {}
+
+    Object.defineProperty(result, 'privateKey', {
+      enumerable: false,
+      value: cryptoKeyPair.privateKey
+    })
+
+    Object.defineProperty(result, 'publicKey', {
+      enumerable: false,
+      value: cryptoKeyPair.publicKey
+    })
+
+    return result
+  }
+
+  /**
+   * setJwkKeyPair
+   */
+  setJwkKeyPair (result) {
+    return Promise.all([
+      crypto.subtle.exportKey('jwk', result.privateKey),
+      crypto.subtle.exportKey('jwk', result.publicKey)
+    ])
+    .then(jwks => {
+      let [privateJwk, publicJwk] = jwks
+
+      result.privateJwk = Object.assign({
+        kid: base64url(Buffer.from(crypto.getRandomValues(new Uint8Array(8))))
+      }, privateJwk)
+
+      result.publicJwk = Object.assign({
+        kid: base64url(Buffer.from(crypto.getRandomValues(new Uint8Array(8))))
+      }, publicJwk)
+
+      return result
+    })
+  }
+}
+
+/**
+ * Export
+ */
+module.exports = EcdsaKeyPair
