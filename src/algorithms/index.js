ff2b70bec7fa60213519ea84f415593fc3d2acf6
Christian Smith <smith@anvil.io>
2017-06-19 18:14:26 -0400

    feat(algorithms): register KS256, KS384, KS512 for generate and import ops

--- src/algorithms/index.js
+++ src/algorithms/index.js
@@ -3,6 +3,7 @@
  */
 const SupportedAlgorithms = require('./SupportedAlgorithms')
 const RsaKeyPair = require('./RsaKeyPair')
+const EcdsaKeyPair = require('./EcdsaKeyPair')
 
 /**
  * Supported Algorithms
@@ -19,6 +20,15 @@ supportedAlgorithms.define('RS256', 'importKey', RsaKeyPair)
 supportedAlgorithms.define('RS384', 'importKey', RsaKeyPair)
 supportedAlgorithms.define('RS512', 'importKey', RsaKeyPair)
 
+/**
+ * ECDSA
+ */
+supportedAlgorithms.define('KS256', 'generateKey', EcdsaKeyPair)
+supportedAlgorithms.define('KS384', 'generateKey', EcdsaKeyPair)
+supportedAlgorithms.define('KS512', 'generateKey', EcdsaKeyPair)
+supportedAlgorithms.define('KS256', 'importKey', EcdsaKeyPair)
+supportedAlgorithms.define('KS384', 'importKey', EcdsaKeyPair)
+supportedAlgorithms.define('KS512', 'importKey', EcdsaKeyPair)
 
 /**
  * Export
